54c569081ec7ed22496e2bdc155c9db8c5443e7e
ganttArt <gantt.art@gmail.com>
2019-09-27 15:12:18 -0700

    Add donation page added, navigation elements added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,9 +15,29 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/add_donation', methods=['GET', 'POST'])
+def add_donation():
+    if request.method == 'POST':
+        try: #add a new donor
+            donor = Donor(name=request.form['name'])
+            donor.save()
+
+            donation = Donation(value=request.form['amount'], donor=donor)
+            donation.save()
+
+        except Exception as exc:
+            print(exc)
+            print('donor already in database')
+            donation = Donation(value=request.form['amount'],
+                                donor=Donor.select().where(Donor.name == request.form['name']).get())
+            donation.save()
+
+        return redirect(url_for('all'))
+
+    else:
+        return render_template('add_donation.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
